9176ba72752c6ab925ad57dd6797c3dd162783d7
jegranbacka <jan-erik.granbacka@solita.fi>
2014-12-18 17:27:25 +0200

    Demoharjoittelupalaverin korjaukset
    - Tila: käynnissä -> keskeneräinen
    - Jos hakemuksen alkamispvm tulevaisuudessa -> Tila:Ei käynnissä
    - Tilat poistettu hakemuskaudenhallintanäytöstä

--- app/scripts/controllers/lahHakemukset.js
+++ app/scripts/controllers/lahHakemukset.js
@@ -11,23 +11,39 @@
 angular.module('jukufrontApp')
   .controller('LahHakemuksetCtrl', function ($scope, $location, HakemusFactory) {
 
-    var loadData = function () {
+    $scope.loadData = function () {
       HakemusFactory.query(function (data) {
         var hakemuksetTmp = [];
         _(angular.fromJson(data)).forEach(function (hakemusvuosi) {
+          var nykyhetki = Number(new Date());
+          var avustushakemusAlkupvm = Number(new Date(_.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakuaika.alkupvm));
+          var maksatushakemus1Alkupvm = Number(new Date(_.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).hakuaika.alkupvm));
+          var maksatushakemus2Alkupvm = Number(new Date(_.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakuaika.alkupvm));
+          var avustushakemusTilatunnus = _.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakemustilatunnus;
+          var maksatushakemus1Tilatunnus = _.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).hakemustilatunnus;
+          var maksatushakemus2Tilatunnus = _.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakemustilatunnus;
+          if (avustushakemusAlkupvm>nykyhetki){
+            avustushakemusTilatunnus = 'FEK';
+          }
+          if (maksatushakemus1Alkupvm>nykyhetki){
+            maksatushakemus1Tilatunnus = 'FEK';
+          }
+          if (maksatushakemus2Alkupvm>nykyhetki){
+            maksatushakemus2Tilatunnus = 'FEK';
+          }
           hakemuksetTmp.push({
             'vuosi': hakemusvuosi.vuosi,
             'avustushakemusId': _.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).id,
-            'avustushakemusTilatunnus': _.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakemustilatunnus,
-            'avustushakemusAlkupvm': Number(new Date(_.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakuaika.alkupvm)),
+            'avustushakemusTilatunnus': avustushakemusTilatunnus,
+            'avustushakemusAlkupvm': avustushakemusAlkupvm,
             'avustushakemusLoppupvm': Number(new Date(_.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakuaika.loppupvm)),
             'maksatushakemus1Id': _.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).id,
-            'maksatushakemus1Tilatunnus': _.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).hakemustilatunnus,
-            'maksatushakemus1Alkupvm': Number(new Date(_.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).hakuaika.alkupvm)),
+            'maksatushakemus1Tilatunnus': maksatushakemus1Tilatunnus,
+            'maksatushakemus1Alkupvm': maksatushakemus1Alkupvm,
             'maksatushakemus1Loppupvm': Number(new Date(_.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).hakuaika.loppupvm)),
             'maksatushakemus2Id': _.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).id,
-            'maksatushakemus2Tilatunnus': _.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakemustilatunnus,
-            'maksatushakemus2Alkupvm': Number(new Date(_.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakuaika.alkupvm)),
+            'maksatushakemus2Tilatunnus': maksatushakemus2Tilatunnus,
+            'maksatushakemus2Alkupvm': maksatushakemus2Alkupvm,
             'maksatushakemus2Loppupvm': Number(new Date(_.find(hakemusvuosi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakuaika.loppupvm))
           });
         });
@@ -41,6 +57,6 @@ angular.module('jukufrontApp')
       $location.path('/l/hakemus/'+hakemusId);
     };
 
-    loadData();
+    $scope.loadData();
 
   });
--- app/scripts/controllers/utils.js
+++ app/scripts/controllers/utils.js
@@ -34,7 +34,7 @@ angular.module('jukufrontApp')
           var htmlText = '<span class="label label-default">Unknown</span>';
           switch (tilatunnus) {
             case 'K':
-              htmlText = '<span class="label label-warning">Käynnissä</span>';
+              htmlText = '<span class="label label-warning">Keskeneräinen</span>';
               break;
             case 'V':
               htmlText = '<span class="label label-danger">Vireillä</span>';
@@ -54,48 +54,13 @@ angular.module('jukufrontApp')
             case 'M':
               htmlText = '<span class="label label-default">Maksettu</span>';
               break;
-          }
-          element.replaceWith(htmlText);
-        });
-      }
-    };
-  })
-  .directive('hakemusButton', function () {
-    return {
-      scope: {
-        tila: '=hakemusButtonTila'
-      },
-      restrict: 'E',
-      link: function (scope, element, attrs) {
-        attrs.$observe('tila', function (tilatunnus) {
-          var htmlText = '<span class="button btn-default">Unknown</span>';
-          switch (tilatunnus) {
-            case 'K':
-              htmlText = '<span class="button btn-warning">Keskeneräinen</span>';
-              break;
-            case 'V':
-              htmlText = '<span class="button btn-danger">Vireillä</span>';
-              break;
-            case 'T':
-              htmlText = '<span class="button btn-success">Tarkastettu</span>';
-              break;
-            case 'T0':
-              htmlText = '<span class="button btn-info">Täydennettävää</span>';
-              break;
-            case 'TV':
-              htmlText = '<span class="button btn-danger">Täydennetty</span>';
-              break;
-            case 'P':
-              htmlText = '<span class="button btn-success">Päätetty</span>';
-              break;
-            case 'M':
-              htmlText = '<span class="button btn-default">Maksettu</span>';
+            case 'FEK':
+              htmlText = '<span class="label label-default">Ei käynnissä</span>';
               break;
           }
           element.replaceWith(htmlText);
         });
       }
     };
-  }
-)
+  });
 
