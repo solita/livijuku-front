6250f79f75a2612ba9120d4cd46ac1ff9c6397c7
jegranbacka <jan-erik.granbacka@solita.fi>
2015-01-12 15:07:39 +0200

    Käyttäjä-cookie asetetaan omilla sivuilla

--- test/e2e/KT0201.js
+++ test/e2e/KT0201.js
@@ -1,14 +1,13 @@
 describe('Selenium Test Case', function() {
 
   beforeEach(function() {
-    browser.driver.get("http://livijuku.local.solita.fi:9000/");
-    browser.manage().addCookie('oam-remote-user', 'harri', '/', 'livijuku.local.solita.fi');
+    browser.driver.get("http://localhost:9000/harri.html");
   });
 
   it('should execute test case without errors', function() {
     var text, value, bool, source, url, title;
     var TestVars = {};
-    browser.get("http://livijuku.local.solita.fi:9000/");
+    browser.get("http://localhost:9000/");
     element(by.css("div.panel-body.clickable")).click();
     text = element(by.tagName('html')).getText();
     expect(text).toContain("" + "PSA:n mukaisen liikenteen hankinta");
