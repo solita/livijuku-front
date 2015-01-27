5002e86c5e8532e8fbf78f86390b1b7a1631b182
jegranbacka <jan-erik.granbacka@solita.fi>
2015-01-27 15:54:14 +0200

    Direktiivi eroitettu omaan tiedostoon

--- app/scripts/controllers/yleinen/paanaytto.js
+++ app/scripts/controllers/yleinen/paanaytto.js
@@ -13,7 +13,7 @@ angular.module('jukufrontApp')
           .success(function (data) {
             $rootScope.user = data;
             $rootScope.userOrganisaatio = _.find($rootScope.organisaatiot, {'id': $rootScope.user.organisaatioid}).nimi;
-            statusService.ok('KayttajaService.hae()', 'Käyttäjätiedot haettu onnistuneesti.' );
+            statusService.ok('KayttajaService.hae()', 'Käyttäjätiedot haettu onnistuneesti.');
           })
           .error(function (data) {
             statusService.virhe('KayttajaService.hae()', data);
@@ -23,44 +23,4 @@ angular.module('jukufrontApp')
         statusService.virhe('OrganisaatioService.hae()', data);
       });
   }]
-)
-  .directive('hakemusLabel', function () {
-    return {
-      scope: {
-        tila: '=hakemusLabelTila'
-      },
-      restrict: 'E',
-      link: function (scope, element, attrs) {
-        attrs.$observe('tila', function (tilatunnus) {
-          var htmlText = '<span class="label label-default">Unknown</span>';
-          switch (tilatunnus) {
-            case 'K':
-              htmlText = '<span class="label label-warning">Keskeneräinen</span>';
-              break;
-            case 'V':
-              htmlText = '<span class="label label-danger">Vireillä</span>';
-              break;
-            case 'T':
-              htmlText = '<span class="label label-success">Tarkastettu</span>';
-              break;
-            case 'T0':
-              htmlText = '<span class="label label-info">Täydennettävää</span>';
-              break;
-            case 'TV':
-              htmlText = '<span class="label label-danger">Täydennetty</span>';
-              break;
-            case 'P':
-              htmlText = '<span class="label label-success">Päätetty</span>';
-              break;
-            case 'M':
-              htmlText = '<span class="label label-default">Maksettu</span>';
-              break;
-            case 'FEK':
-              htmlText = '<span class="label label-default">Ei käynnissä</span>';
-              break;
-          }
-          element.replaceWith(htmlText);
-        });
-      }
-    };
-  });
+);
---
+++ app/scripts/directives/hakemuslabel.js
@@ -0,0 +1,43 @@
+'use strict';
+angular.module('jukufrontApp')
+  .directive('hakemusLabel', function () {
+    return {
+      scope: {
+        tila: '=hakemusLabelTila'
+      },
+      restrict: 'E',
+      link: function (scope, element, attrs) {
+        attrs.$observe('tila', function (tilatunnus) {
+          var htmlText = '<span class="label label-default">Unknown</span>';
+          switch (tilatunnus) {
+            case 'K':
+              htmlText = '<span class="label label-warning">Keskeneräinen</span>';
+              break;
+            case 'V':
+              htmlText = '<span class="label label-danger">Vireillä</span>';
+              break;
+            case 'T':
+              htmlText = '<span class="label label-success">Tarkastettu</span>';
+              break;
+            case 'T0':
+              htmlText = '<span class="label label-info">Täydennettävää</span>';
+              break;
+            case 'TV':
+              htmlText = '<span class="label label-danger">Täydennetty</span>';
+              break;
+            case 'P':
+              htmlText = '<span class="label label-success">Päätetty</span>';
+              break;
+            case 'M':
+              htmlText = '<span class="label label-default">Maksettu</span>';
+              break;
+            case 'FEK':
+              htmlText = '<span class="label label-default">Ei käynnissä</span>';
+              break;
+          }
+          element.replaceWith(htmlText);
+        });
+      }
+    };
+  }
+);
