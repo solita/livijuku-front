2c4f542c71a9849a9f0e38dab56eac25254e2e34
Petri Sirkkala <sirpete@iki.fi>
2015-02-12 22:13:20 +0200

    Salli imagen korvaaminen. Käytä työhakemiston kopiota. Asenna tarvittavat binäärit, jos ne puuttuvat. Sudo tarvittaessa.

--- run-ci-build.sh
+++ run-ci-build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 PROJECT=livijuku-front-build
+CONTAINER_HOME=/home/jenkins-slave
+
 read -r -d '' SCRIPT <<- End
-    Xvfb :1 -ac &
+    command -v Xvfb >/dev/null 2>&1 || (sudo apt-get update && sudo apt-get -y install Xvfb)
+    sudo Xvfb :1 -ac &
     export DISPLAY=:1
-    cd $PROJECT
+    rsync -aP --delete --exclude node_modules --exclude bower_components --exclude .git $CONTAINER_HOME/$PROJECT/ $CONTAINER_HOME/$PROJECT.local/
+    cd $CONTAINER_HOME/$PROJECT.local
+    command -v bower >/dev/null 2>&1 || sudo npm install -g bower
+    command -v grunt >/dev/null 2>&1 || sudo npm install -g grunt-cli
     eval ./ci.sh
 End
 
-IMG=sirkkalap/jenkins-swarm-slave-nlm:java8
-MOUNT="-v $(pwd):/home/jenkins-slave/$PROJECT"
-ENVS="-e "USER=$USER""
+IMG=${1:-sirkkalap/jenkins-swarm-slave-nlm:java8}
+MOUNT="-v $(pwd):$CONTAINER_HOME/$PROJECT"
+ENVS="-e "USER=$USER" -e "HOME=$CONTAINER_HOME""
 NAME="--name $PROJECT"
 OPTS="-it --sig-proxy=true"
 
@@ -19,8 +25,9 @@ if [ ! -z $VOLFROM ]; then
     VOLFROM="--volumes-from $VOLFROM"
 else
     echo "To make persistent volume for build (cache) use:"
-    echo "docker run --name $PROJECT-volume $MOUNT -v /home/jenkins-slave $IMG true"
+    echo "docker run --name $PROJECT-volume $MOUNT -v $CONTAINER_HOME $IMG true"
 fi
 
 docker rm -f $PROJECT 2>/dev/null
 docker run $OPTS $NAME $VOLFROM $ENVS $MOUNT $IMG /bin/bash -c "$SCRIPT"
+
