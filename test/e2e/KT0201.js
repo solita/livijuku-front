cf73b8136ae6262963e58f7d3fbdbef5df58bbbb
Petri Sirkkala <sirpete@iki.fi>
2015-02-14 19:49:18 +0200

    LIVIJUKU-94 Testit uusiksi. Toiminnallisuus ei vielä riitä.

--- test/e2e/KT0201.js
+++ test/e2e/KT0201.js
@@ -1,15 +1,43 @@
 describe('Selenium Test Case', function() {
 
+  var snapshot = function() {
+    console.log('TODO: Kannan snapshot');
+  };
+
+  var restore = function() {
+    console.log('TODO: Kannan restore');
+  };
+
+  //beforeAll(function() {
+  //  snapshot();
+  //});
+  //
+  //afterAll( function() {
+  //  restore();
+  //});
+
   beforeEach(function() {
     browser.get("http://localhost:9000/harri.html");
   });
 
-  it('should execute test case without errors', function() {
-    var text, value, bool, source, url, title;
-    var TestVars = {};
-    browser.get("http://localhost:9000/");
-    element(by.css("div.panel-body.clickable")).click();
+  it('Käsittelijä avaa hakemuskauden. Hakemuskausi avautuu.', function() {
+    snapshot();
+    browser.get("/katri.html");
+    //var kayttajanNimi = element(by.xpath('//li[@class="navbaruser"]/p[1]'));
+    //expect(kayttajanNimi.getText().toContain('Katri Käsittelijä'));
+    element(by.partialLinkText("Hakemukset")).click();
+    element(by.partialLinkText("Hakemuskauden hallinta")).click();
+    //browser.pause();
+    element(by.partialLinkText("Käynnistä hakemuskausi")).click();
+    expect(element(by.css('p.panel-title')).getText()).toContain('Avustushakemus');
+  });
+
+  it('Hakija avaa avustushakemuslomakkeen. Avustusakemuslomakkeella lukee: "PSA:n mukaisen liikenteen hankinta"', function() {
+    var text;
+    browser.get("/");
+    element(by.partialLinkText("Omat hakemukset")).click();
     text = element(by.tagName('html')).getText();
     expect(text).toContain("" + "PSA:n mukaisen liikenteen hankinta");
+    restore();
   });
 });
