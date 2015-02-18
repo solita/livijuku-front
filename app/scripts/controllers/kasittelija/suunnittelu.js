8ecb69ca6e4e1fc36ba2aaad689f3e604f442382
jegranbacka <jan-erik.granbacka@solita.fi>
2015-02-18 13:35:12 +0200

    LIVIJUKU-139 Määrärahojen antaminen, ensimmäinen versio

--- app/scripts/controllers/kasittelija/suunnittelu.js
+++ app/scripts/controllers/kasittelija/suunnittelu.js
@@ -1,15 +1,32 @@
 'use strict';
 
 angular.module('jukufrontApp')
-  .controller('KasittelijaSuunnitteluCtrl', ['$rootScope', '$scope', '$location', '$routeParams', 'HakemusService', 'SuunnitteluService', 'StatusService', function ($rootScope, $scope, $location, $routeParams, HakemusService, SuunnitteluService, StatusService) {
+  .controller('KasittelijaSuunnitteluCtrl', ['$rootScope', '$scope', '$location', '$routeParams', 'HakemuskausiService', 'HakemusService', 'SuunnitteluService', 'StatusService', function ($rootScope, $scope, $location, $routeParams, HakemuskausiService, HakemusService, SuunnitteluService, StatusService) {
 
     $scope.lajitunnus = $routeParams.lajitunnus;
     $scope.tyyppi = $routeParams.tyyppi;
     $scope.vanhaArvo = 0;
     $scope.vuosi = $routeParams.vuosi;
 
+    function haeMaararahat() {
+      HakemuskausiService.haeMaararaha($scope.vuosi, $scope.lajitunnus)
+        .success(function (data) {
+          if (data != null) {
+            $scope.maararaha = data.maararaha;
+            $scope.ylijaama = data.ylijaama;
+          } else {
+            $scope.maararaha = 0;
+            $scope.ylijaama = 0;
+          }
+          $scope.jaettavaraha = $scope.maararaha + $scope.ylijaama;
+        })
+        .error(function (data) {
+          StatusService.virhe('HakemuskausiService.haeMaararaha(' + $scope.vuosi + ',' + $scope.lajitunnus + ')', data);
+        });
+    }
+
     function haeSuunnitteluData() {
-      SuunnitteluService.hae($routeParams.vuosi, $routeParams.tyyppi)
+      SuunnitteluService.hae($scope.vuosi, $scope.tyyppi)
         .success(function (data) {
           var hakemuksetSuunnitteluTmp = [];
           var organisaatiolajitunnus = "";
@@ -20,7 +37,7 @@ angular.module('jukufrontApp')
               organisaatiolajitunnus = _.find($rootScope.organisaatiot, {'id': hakemus.organisaatioid}).lajitunnus;
               if (organisaatiolajitunnus == $scope.lajitunnus) {
                 var muutos = 0;
-                if (hakemus.hakemustilatunnus === 'T'||hakemus.hakemustilatunnus === 'P') {
+                if (hakemus.hakemustilatunnus === 'T' || hakemus.hakemustilatunnus === 'P') {
                   $scope.haettuAvustusSum = $scope.haettuAvustusSum + hakemus['haettu-avustus'];
                   $scope.myonnettavaAvustusSum = $scope.myonnettavaAvustusSum + hakemus['myonnettava-avustus'];
                   muutos = hakemus['myonnettava-avustus'] - hakemus['haettu-avustus'];
@@ -66,5 +83,38 @@ angular.module('jukufrontApp')
           });
       }
     };
+
+    $scope.paivitaKokonaismaararaha = function (arvo) {
+      var muuttunut = false;
+      if (arvo == 'maararaha') {
+        if (isNaN($scope.maararaha)) {
+          haeMaararahat();
+        } else {
+          muuttunut = $scope.maararaha != $scope.vanhaArvo;
+        }
+      }
+      if (arvo == 'ylijaama') {
+        if (isNaN($scope.ylijaama)) {
+          haeMaararahat();
+        } else {
+          muuttunut = $scope.ylijaama != $scope.vanhaArvo;
+        }
+      }
+      if (muuttunut) {
+        var maararahadata = {
+          'maararaha': $scope.maararaha,
+          'ylijaama': $scope.ylijaama
+        };
+        HakemuskausiService.paivitaMaararaha($scope.vuosi, $scope.lajitunnus, maararahadata)
+          .success(function () {
+            haeMaararahat();
+          })
+          .error(function (data) {
+            StatusService.virhe('HakemuskausiService.paivitaMaararaha(' + $scope.vuosi + ',' + $scope.lajitunnus + ',' + maararahadata + ')', data);
+          });
+      }
+    };
+
+    haeMaararahat();
     haeSuunnitteluData();
   }]);
--- app/scripts/services/hakemuskausi.js
+++ app/scripts/services/hakemuskausi.js
@@ -10,8 +10,22 @@ angular.module('services.hakemuskausi', [])
       haeHakuohje: function (vuosi) {
         return $http.get('api/hakemuskausi/'+vuosi+'/hakuohje');
       },
+      haeMaararaha: function (vuosi, organisaatiolajitunnus) {
+        return $http.get('api/maararaha/'+vuosi+'/'+organisaatiolajitunnus);
+      },
       luoUusi: function(vuosi) {
         return $http.post('api/hakemuskausi', {vuosi: vuosi});
+      },
+      paivitaMaararaha: function (vuosi, organisaatiolajitunnus, maararahadata) {
+        var req = {
+          method: 'PUT',
+          url: 'api/maararaha/'+vuosi+'/'+organisaatiolajitunnus,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: maararahadata
+        };
+        return $http(req);
       }
     };
   }]);
