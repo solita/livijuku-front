e6592815ce1b33c0d0cf6d0eec40bb5d8e957c53
jegranbacka <jan-erik.granbacka@solita.fi>
2015-03-03 09:51:52 +0200

    Hakemuskauden hallinta/hakuaikojen muutos väliversio
    Automaattista testausta varten (input type=file)

--- app/scripts/controllers/kasittelija/hakemuskaudenHallinta.js
+++ app/scripts/controllers/kasittelija/hakemuskaudenHallinta.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('jukufrontApp')
+  // this is the important bit:
+  .directive('datepickerPopup', function (){
+    return {
+      restrict: 'EAC',
+      require: 'ngModel',
+      link: function(scope, element, attr, controller) {
+        //remove the default formatter from the input directive to prevent conflict
+        controller.$formatters.shift();
+      }
+    }
+  })
   .controller('KasittelijaHakemuskaudenHallintaCtrl', ['$scope', '$location', '$route', '$log', 'HakemuskausiService', 'StatusService', '$upload', function ($scope, $location, $route, $log, HakemuskausiService, StatusService, $upload) {
 
     function haeHakemuskaudet() {
@@ -16,13 +27,19 @@ angular.module('jukufrontApp')
                 'tilatunnus': hakemuskausi.tilatunnus,
                 'avustushakemusTilatunnus': '',
                 'avustushakemusAlkupvm': '',
+                'avustushakemusAlkupvmKalenteri': false,
                 'avustushakemusLoppupvm': '',
+                'avustushakemusLoppupvmKalenteri': false,
                 'maksatushakemus1Tilatunnus': '',
                 'maksatushakemus1Alkupvm': '',
+                'maksatushakemus1AlkupvmKalenteri': false,
                 'maksatushakemus1Loppupvm': '',
+                'maksatushakemus1LoppupvmKalenteri': false,
                 'maksatushakemus2Tilatunnus': '',
                 'maksatushakemus2Alkupvm': '',
-                'maksatushakemus2Loppupvm': ''
+                'maksatushakemus2AlkupvmKalenteri': false,
+                'maksatushakemus2Loppupvm': '',
+                'maksatushakemus2LoppupvmKalenteri': false
               });
             } else {
               hakemuskaudetTmp.push({
@@ -30,13 +47,19 @@ angular.module('jukufrontApp')
                 'tilatunnus': hakemuskausi.tilatunnus,
                 'avustushakemusTilatunnus': _.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakemustilatunnus,
                 'avustushakemusAlkupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakuaika.alkupvm)),
+                'avustushakemusAlkupvmKalenteri': false,
                 'avustushakemusLoppupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakuaika.loppupvm)),
+                'avustushakemusLoppupvmKalenteri': false,
                 'maksatushakemus1Tilatunnus': _.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).hakemustilatunnus,
                 'maksatushakemus1Alkupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).hakuaika.alkupvm)),
+                'maksatushakemus1AlkupvmKalenteri': false,
                 'maksatushakemus1Loppupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).hakuaika.loppupvm)),
+                'maksatushakemus1LoppupvmKalenteri': false,
                 'maksatushakemus2Tilatunnus': _.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakemustilatunnus,
                 'maksatushakemus2Alkupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakuaika.alkupvm)),
-                'maksatushakemus2Loppupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakuaika.loppupvm))
+                'maksatushakemus2AlkupvmKalenteri': false,
+                'maksatushakemus2Loppupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakuaika.loppupvm)),
+                'maksatushakemus2LoppupvmKalenteri': false
               });
             }
           });
@@ -48,13 +71,19 @@ angular.module('jukufrontApp')
               'uusi': true,
               'avustushakemusTilatunnus': '',
               'avustushakemusAlkupvm': '',
+              'avustushakemusAlkupvmKalenteri': false,
               'avustushakemusLoppupvm': '',
+              'avustushakemusLoppupvmKalenteri': false,
               'maksatushakemus1Tilatunnus': '',
               'maksatushakemus1Alkupvm': '',
+              'maksatushakemus1AlkupvmKalenteri': false,
               'maksatushakemus1Loppupvm': '',
+              'maksatushakemus1LoppupvmKalenteri': false,
               'maksatushakemus2Tilatunnus': '',
               'maksatushakemus2Alkupvm': '',
-              'maksatushakemus2Loppupvm': ''
+              'maksatushakemus2AlkupvmKalenteri': false,
+              'maksatushakemus2Loppupvm': '',
+              'maksatushakemus2LoppupvmKalenteri': false
             });
           }
           $scope.hakemuskaudet = _.sortBy(hakemuskaudetTmp, 'vuosi').reverse();
@@ -62,6 +91,11 @@ angular.module('jukufrontApp')
         .error(function (data) {
           StatusService.virhe('HakemuskausiService.hae()', data);
         });
+    };
+
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
     };
 
     $scope.luoUusiHakemuskausi = function (vuosi) {
@@ -73,6 +107,11 @@ angular.module('jukufrontApp')
         .error(function (data) {
           StatusService.virhe('HakemuskausiService.luoUusi(' + vuosi + ')', data);
         });
+    };
+
+    $scope.avaaKalenteri = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
     };
 
     $scope.upload = function (tiedostot, vuosi) {
