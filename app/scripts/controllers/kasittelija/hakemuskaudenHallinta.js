aae78bdf80e20f47eafef4984e0d3771cad84cf0
jegranbacka <jan-erik.granbacka@solita.fi>
2015-03-04 11:16:35 +0200

    Hakemuskauden hallinta/hakuaikojen muutos väliversio 3

--- app/scripts/controllers/kasittelija/hakemuskaudenHallinta.js
+++ app/scripts/controllers/kasittelija/hakemuskaudenHallinta.js
@@ -8,23 +8,24 @@ angular.module('jukufrontApp')
       HakemuskausiService.hae()
         .success(function (data) {
           var hakemuskaudetTmp = [];
+          var nykyhetki = Number(new Date());
           _(angular.fromJson(data)).forEach(function (hakemuskausi) {
             if (hakemuskausi.tilatunnus == "A" || hakemuskausi.tilatunnus == "0") {
               hakemuskaudetTmp.push({
                 'vuosi': hakemuskausi.vuosi,
                 'uusi': true,
                 'tilatunnus': hakemuskausi.tilatunnus,
-                'avustushakemusTilatunnus': '',
+                'avustushakemusKaynnissa': false,
                 'avustushakemusAlkupvm': '2014-09-01T00:00:00.000Z',
                 'avustushakemusAlkupvmKalenteri': false,
                 'avustushakemusLoppupvm': '2014-10-01T00:00:00.000Z',
                 'avustushakemusLoppupvmKalenteri': false,
-                'maksatushakemus1Tilatunnus': '',
+                'maksatushakemus1Kaynnissa': false,
                 'maksatushakemus1Alkupvm': '2014-09-01T00:00:00.000Z',
                 'maksatushakemus1AlkupvmKalenteri': false,
                 'maksatushakemus1Loppupvm': '2014-09-01T00:00:00.000Z',
                 'maksatushakemus1LoppupvmKalenteri': false,
-                'maksatushakemus2Tilatunnus': '',
+                'maksatushakemus2Kaynnissa': false,
                 'maksatushakemus2Alkupvm': '2014-09-01T00:00:00.000Z',
                 'maksatushakemus2AlkupvmKalenteri': false,
                 'maksatushakemus2Loppupvm': '2014-09-01T00:00:00.000Z',
@@ -34,18 +35,17 @@ angular.module('jukufrontApp')
               hakemuskaudetTmp.push({
                 'vuosi': hakemuskausi.vuosi,
                 'tilatunnus': hakemuskausi.tilatunnus,
-                'avustushakemusTilatunnus': _.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakemustilatunnus,
+                'avustushakemusKaynnissa': nykyhetki>Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakuaika.alkupvm)),
                 'avustushakemusAlkupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakuaika.alkupvm)),
-                'avustushakemusAlkupvmAlkup': new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakuaika.alkupvm),
                 'avustushakemusAlkupvmKalenteri': false,
                 'avustushakemusLoppupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'AH0'}).hakuaika.loppupvm)),
                 'avustushakemusLoppupvmKalenteri': false,
-                'maksatushakemus1Tilatunnus': _.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).hakemustilatunnus,
+                'maksatushakemus1Kaynnissa': nykyhetki>Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).hakuaika.alkupvm)),
                 'maksatushakemus1Alkupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).hakuaika.alkupvm)),
                 'maksatushakemus1AlkupvmKalenteri': false,
                 'maksatushakemus1Loppupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH1'}).hakuaika.loppupvm)),
                 'maksatushakemus1LoppupvmKalenteri': false,
-                'maksatushakemus2Tilatunnus': _.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakemustilatunnus,
+                'maksatushakemus2Kaynnissa': nykyhetki>Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakuaika.alkupvm)),
                 'maksatushakemus2Alkupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakuaika.alkupvm)),
                 'maksatushakemus2AlkupvmKalenteri': false,
                 'maksatushakemus2Loppupvm': Number(new Date(_.find(hakemuskausi.hakemukset, {'hakemustyyppitunnus': 'MH2'}).hakuaika.loppupvm)),
@@ -81,7 +81,14 @@ angular.module('jukufrontApp')
       $event.stopPropagation();
     };
 
-    $scope.muokkaaHakuaikojaVuosi = '';
+    $scope.muokkaaHakuaikojaVuosi = null;
+    $scope.avustushakemusAlkupvm = null;
+    $scope.avustushakemusLoppupvm = null;
+    $scope.maksatushakemus1Alkupvm = null;
+    $scope.maksatushakemus1Loppupvm = null;
+    $scope.maksatushakemus2Alkupvm = null;
+    $scope.maksatushakemus2Loppupvm = null;
+
 
     $scope.asetaMuokkaaHakuaikojaVuosi = function (vuosi, ah0alkupvm, ah0loppupvm, mh1alkupvm, mh1loppupvm, mh2alkupvm, mh2loppupvm) {
       $scope.avustushakemusAlkupvm = ah0alkupvm;
@@ -95,9 +102,20 @@ angular.module('jukufrontApp')
 
     $scope.tallennaHakuajat = function (vuosi) {
       console.log("Tallenna hakuajat vuosi:" + vuosi);
-      console.log("Alkupvm:" + $scope.avustushakemusAlkupvm + ' nd:' + new Date($scope.avustushakemusAlkupvm).toISOString());
-      console.log("Loppupvm:" + $scope.avustushakemusLoppupvm);
-      $scope.muokkaaHakuaikojaVuosi = '';
+      console.log("AH0_Alkupvm:" + $scope.avustushakemusAlkupvm + ' date:' + new Date($scope.avustushakemusAlkupvm).toISOString());
+      console.log("AH0_Loppupvm:" + $scope.avustushakemusLoppupvm + ' date:' + new Date($scope.avustushakemusLoppupvm).toISOString());
+      console.log("MH1_Alkupvm:" + $scope.maksatushakemus1Alkupvm + ' date:' + new Date($scope.maksatushakemus1Alkupvm).toISOString());
+      console.log("MH1_Loppupvm:" + $scope.maksatushakemus1Loppupvm + ' date:' + new Date($scope.maksatushakemus1Loppupvm).toISOString());
+      console.log("MH2_Alkupvm:" + $scope.maksatushakemus2Alkupvm + ' date:' + new Date($scope.maksatushakemus2Alkupvm).toISOString());
+      console.log("MH2_Loppupvm:" + $scope.maksatushakemus2Loppupvm + ' date:' + new Date($scope.maksatushakemus2Loppupvm).toISOString());
+      $scope.muokkaaHakuaikojaVuosi = null;
+      $scope.avustushakemusAlkupvm = null;
+      $scope.avustushakemusLoppupvm = null;
+      $scope.maksatushakemus1Alkupvm = null;
+      $scope.maksatushakemus1Loppupvm = null;
+      $scope.maksatushakemus2Alkupvm = null;
+      $scope.maksatushakemus2Loppupvm = null;
+      $scope.muokkaaHakuaikojaVuosi = null;
     };
 
 
