f0e9a1c8971c0b90ac2ca5e8fdb02936c55a6724
Petri Sirkkala <sirpete@iki.fi>
2015-03-05 16:48:30 +0200

    LIVIJUKU-129 Testaus: Käsittelijä avaa uuden avustuskauden. Toimiva create_restorepoint / revert_to testitoteutuksessa.

--- test/e2e/KT0201.js
+++ test/e2e/KT0201.js
@@ -2,12 +2,8 @@ describe('Selenium Test Case', function() {
 
   var path = require('path');
 
-  var snapshot = function() {
-    console.log('TODO: Kannan snapshot');
-  };
-
-  var restore = function() {
-    console.log('TODO: Kannan restore');
+  var makeGet = function (d) {
+    return require('http').get(d);
   };
 
   var unhideFileInputs = function () {
@@ -17,25 +13,19 @@ describe('Selenium Test Case', function() {
   };
 
   beforeEach(function() {
-    // snapshot();
     // Lokitetaan selaimen konsolotuloste
     browser.manage().logs().get('browser').then(function(browserLog) {
       console.log('log: ' + require('util').inspect(browserLog));
     });
   });
 
-  //afterAll( function() {
-  //  restore();
-  //});
-
-  //var setup_data = function (d) {
-  //  console.log("Data:");
-  //  console.log(d);
-  //
-  //  return "OKOKOKOKOKOK";//$http.get(data);
-  //};
 
   it('Käsittelijä lisää hakuohjeen hakukaudelle. Hakuohjeen lisääminen onnistuu.', function() {
+    //createRestorePoint("beforeAll");
+    browser.wait( function() {
+      return makeGet('http://juku:juku@127.0.0.1:50000/juku/testing.create_restorepoint?restorepoint=beforeAll');
+    }, 30000);
+
     browser.get("/katri.html");
     var kayttajanNimi = element(by.xpath('//li[@class="navbaruser"]/p[1]'));
     expect(kayttajanNimi.getText()).toContain('Katri Käsittelijä');
@@ -55,6 +45,10 @@ describe('Selenium Test Case', function() {
     }, 30000);
 
     expect(infoBox.getText()).toContain("onnistui.");
+
+    browser.wait( function() {
+      return makeGet('http://juku:juku@127.0.0.1:50000/juku/testing.revert_to?restorepoint=beforeAll');
+    }, 30000);
   });
 
   //it('Hakija avaa avustushakemuslomakkeen. Avustusakemuslomakkeella lukee: "PSA:n mukaisen liikenteen hankinta"', function() {
