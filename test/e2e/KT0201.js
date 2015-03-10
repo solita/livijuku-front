cee987f749991dc8195174ddd29cd54520773097
Petri Sirkkala <sirpete@iki.fi>
2015-03-10 12:14:38 +0200

    LIVIJUKU-125:LV Test: Hakija täydentää tiedot avustushakemuslomakkeelle
    - Avustushakulomakkeen avaus

--- test/e2e/KT0201.js
+++ test/e2e/KT0201.js
@@ -1,31 +1,44 @@
 'use strict';
-describe('Selenium Test Case', function() {
+describe('Selenium Test Case', function () {
 
   var path = require('path');
 
   var db_http_service = process.env.DB_HTTP_SERVICE || "http://juku:juku@localhost:50000";
 
-  var makeGet = function (d) {
-    return require('http').get(d);
-  };
+  function makeGet(url) {
+    return require('http').get(url);
+  }
 
-  var unhideFileInputs = function () {
+  function unhideFileInputs() {
     browser.executeScript(function () {
       $('input[type="file"]').removeClass('hidden-file-input');
     });
-  };
+  }
 
-  beforeEach(function() {
-    // Lokitetaan selaimen konsolotuloste
-    browser.manage().logs().get('browser').then(function(browserLog) {
-      console.log('log: ' + require('util').inspect(browserLog));
-    });
+  function createRestorePoint(restorePointName) {
+    browser.wait(function () {
+      return makeGet(db_http_service + '/juku/testing.create_restorepoint?restorepoint=' + restorePointName);
+    }, 30000);
+  }
+
+  function revertTo(restorePointName) {
+    browser.wait(function () {
+      return makeGet(db_http_service + '/juku/testing.revert_to?restorepoint=' + restorePointName);
+    }, 30000);
+  }
+
+  beforeAll(function() {
+    createRestorePoint("beforeAll");
   });
 
-  it('Tehdään restorepoint "beforeAll", johon palautetaan kanta testien jälkeen', function() {
-    browser.wait( function() {
-      return makeGet(db_http_service + '/juku/testing.create_restorepoint?restorepoint=beforeAll');
-    }, 30000);
+  afterAll(function () {
+    revertTo("beforeAll");
+    // Jos DEBUG on päällä, Lokitetaan selaimen konsolituloste.
+    if (process.env.DEBUG) {
+      browser.manage().logs().get('browser').then(function (browserLog) {
+        console.log('browser console log: ' + require('util').inspect(browserLog));
+      });
+    }
   });
 
   function waitForInfoBox(partialText) {
@@ -34,10 +47,11 @@ describe('Selenium Test Case', function() {
     browser.wait(function () {
       return browser.isElementPresent(infoBox);
     }, 30000);
+
     return infoBox;
   }
 
-  it('Käsittelijä lisää hakuohjeen hakukaudelle. Hakuohjeen lisääminen onnistuu.', function() {
+  it('Käsittelijä lisää hakuohjeen hakukaudelle. Hakuohjeen lisääminen onnistuu.', function () {
     browser.get("/katri.html");
     var kayttajanNimi = element(by.xpath('//li[@class="navbaruser"]/p[1]'));
     expect(kayttajanNimi.getText()).toContain('Katri Käsittelijä');
@@ -46,16 +60,16 @@ describe('Selenium Test Case', function() {
 
     var fileToUpload = 'test.pdf';
     var absolutePath = path.resolve(__dirname, fileToUpload);
-    console.log(absolutePath); // lokitetaan toistaiseksi, kunnes nähdään onko ok Jenkinsissä
     unhideFileInputs();
     browser.$('input[type="file"]').sendKeys(absolutePath);
 
     var infoBox = waitForInfoBox('Hakuohjeen: test.pdf lataus vuodelle:2016 onnistui.');
 
     expect(infoBox.getText()).toContain("Hakuohjeen: test.pdf lataus vuodelle:2016 onnistui.");
+
   });
 
-  it('Käsittelijä avaa hakukauden. Hakukausi avautuu.', function() {
+  it('Käsittelijä avaa hakukauden. Hakukausi avautuu.', function () {
 
     browser.get("/katri.html");
     var kayttajanNimi = element(by.xpath('//li[@class="navbaruser"]/p[1]'));
@@ -71,19 +85,10 @@ describe('Selenium Test Case', function() {
 
   });
 
-  //it('Hakija avaa avustushakemuslomakkeen. Avustusakemuslomakkeella lukee: "PSA:n mukaisen liikenteen hankinta"', function() {
-  //  var text;
-  //  browser.get("/harri.html");
-  //  //browser.pause();
-  //  element(by.partialLinkText("Omat hakemukset")).click();
-  //  text = element(by.tagName('html')).getText();
-  //  expect(text).toContain("" + "PSA:n mukaisen liikenteen hankinta");
-  //  restore();
-  //});
-
-  it('Palautetaan kanta takaisin restorepointiin "beforeAll"', function() {
-    browser.wait( function() {
-      return makeGet(db_http_service + '/juku/testing.revert_to?restorepoint=beforeAll');
-    }, 30000);
+  it('Hakija avaa avustushakemuslomakkeen. Avustusakemuslomakkeella lukee: "PSA:n mukaisen liikenteen hankinta"', function () {
+    browser.get("/harri.html");
+    element(by.partialLinkText("Omat hakemukset")).click();
+    //expect(element(by.xpath("//*[@class='panel-title' and normalize-space(text())='PSA:n mukaisen liikenteen hankinta']"))).toBeDefined();
   });
+
 });
