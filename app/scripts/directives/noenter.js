89e070dc5a6b419408f9800a1e975a2534bedbd2
jegranbacka <jan-erik.granbacka@solita.fi>
2015-04-14 12:12:59 +0300

    LIVIJUKU-194 Liitetiedoston nimen muokkaus avaa file-open dialogin bugi korjattu

--- app/scripts/directives/noenter.js
+++ app/scripts/directives/noenter.js
@@ -5,7 +5,7 @@ angular.module('jukufrontApp')
       element.bind("keydown keypress", function (event) {
         if (event.which === 13) {
           event.preventDefault();
-          var fields = $(this).parents('form:eq(0),body').find('input, textarea, select');
+          var fields = $(this).parents('form:eq(0),body').find('input, textarea, select, a');
           var index = fields.index(this);
           if (index > -1 && (index + 1) < fields.length)
             fields.eq(index + 1).focus();
