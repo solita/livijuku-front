ecdeedc7a7f93795ce4d1dbea25c57025da877eb
jegranbacka <jan-erik.granbacka@solita.fi>
2015-04-22 16:00:55 +0300

    IE:ssä AngularJS http-kutsut haetaan oletuksena cachesta.  Cachen disablointi-korjaus

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -68,13 +68,28 @@ angular
       .when('/k/suunnittelu/:vuosi/:tyyppi/:lajitunnus', {
         templateUrl: 'views/kasittelija/suunnittelu.html',
         controller: 'KasittelijaSuunnitteluCtrl'
-      })
-      .when('/main', {
+      })      .when('/main', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
       .otherwise({
         redirectTo: '/'
       });
+  }])
+  .config(['$httpProvider', function($httpProvider) {
+    //http://stackoverflow.com/questions/16098430/angular-ie-caching-issue-for-http
+    //initialize get if not there
+    if (!$httpProvider.defaults.headers.get) {
+      $httpProvider.defaults.headers.get = {};
+    }
+
+    // Answer edited to include suggestions from comments
+    // because previous version of code introduced browser-related errors
+
+    //disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    // extra
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.Pragma = 'no-cache';
   }]);
 
