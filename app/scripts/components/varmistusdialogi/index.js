8b00e62f3af05a7f3dcd616f982cbc25da6f3d55
jegranbacka <jan-erik.granbacka@solita.fi>
2015-05-20 11:35:39 +0300

    LIVIJUKU-248 Varmistusdialogit tilamuutosten yhteyteen
    Muutettu direktiivistä Rikun komponentti-muotoon

---
+++ app/scripts/components/varmistusdialogi/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+function ModalInstanceCtrl($scope, $modalInstance) {
+  $scope.ok = function () {
+    $modalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+}
+
+ModalInstanceCtrl.inject =  ['$scope', '$modalInstance'];
+
+module.exports = function ($modal) {
+  return {
+    restrict: 'A',
+    replace: true,
+    scope: {
+      operaatio: "&",
+      varmistusotsake: "@",
+      varmistusteksti: "@"
+    },
+    link: function (scope, element, attrs) {
+      element.bind('click', function () {
+        var modalInstance = $modal.open({
+          scope: scope,
+          template: require('./index.html'),
+          controller: ModalInstanceCtrl
+        });
+
+        modalInstance.result.then(function () {
+          scope.operaatio();
+        }, function () {
+          //Cancel
+        });
+      });
+
+    }
+  };
+};
+
+module.exports.$inject = ['$modal'];
