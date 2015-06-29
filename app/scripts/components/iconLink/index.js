f6adaca59a4a996b5e0767e3bb2f03eead747ff6
Riku Rouvila <riku.rouvila@gmail.com>
2015-06-29 12:38:55 +0300

    LIVIJUKU-253
    * Navigaatiolinkeille uusi kompositio ikonilinkeistä
      - Kahta replaceavaa komponenttia ei voi komposioida yhteen joten tämä täytyy tehdä funktioina. Ilman replacea parametrit kuten href ei mene anchor-elementille asti

--- app/scripts/components/iconLink/index.js
+++ app/scripts/components/iconLink/index.js
@@ -1,20 +1,31 @@
 'use strict';
 const transclude = require('utils/transclude');
+const _ = require('lodash');
 
-module.exports = function () {
-  return transclude({
-    scope: {
-      iconFirst: '=',
-      icon: '@'
-    },
-    restrict: 'EA',
-    template: `
-      <a class="icon-link">
-        <span ng-if="iconFirst !== false" class="glyphicon glyphicon-{{icon}}"></span>
-        <span transclude></span>
-        <span ng-if="iconFirst === false" class="glyphicon glyphicon-{{icon}}"></span>
-      </a>
-    `
-  });
-};
 
+function iconLink(opts = {}) {
+
+  const iconFirstCondition = typeof opts.iconFirst === 'undefined' ? 'iconFirst !== false' : opts.iconFirst;
+  const iconLastCondition = typeof opts.iconFirst === 'undefined' ? 'iconFirst === false' : !opts.iconFirst;
+
+  return function() {
+
+    return transclude({
+      scope: {
+        iconFirst: '=',
+        icon: '@'
+      },
+      restrict: 'EA',
+      template: `
+        <a class="icon-link ${opts.className || ''}">
+          <span ng-if="${iconFirstCondition}" class="glyphicon glyphicon-${opts.icon || '{{icon}}'}"></span>
+          <span transclude></span>
+          <span ng-if="${iconLastCondition}" class="glyphicon glyphicon-${opts.icon || '{{icon}}'}"></span>
+        </a>
+      `
+    });
+  };
+}
+
+module.exports = iconLink();
+module.exports.compose = iconLink;
--- app/scripts/components/navigationLink/index.js
+++ app/scripts/components/navigationLink/index.js
@@ -1,22 +1,15 @@
 'use strict';
 
-const transclude = require('utils/transclude');
+const iconLink = require('components/iconLink');
 
-module.exports.next = function() {
-  return transclude({
-    replace: false,
-    template: `
-      <juku-icon-link icon="circle-arrow-right" icon-first="false"></juku-icon-link>
-    `
-  });
-};
+module.exports.next = iconLink.compose({
+  icon: 'arrow-right',
+  iconFirst: false,
+  className: 'view-link'
+});
 
-module.exports.prev = function() {
-  return transclude({
-    replace: false,
-    template: `
-      <juku-icon-link icon="circle-arrow-left"></juku-icon-link>
-    `
-  });
-};
-
+module.exports.prev = iconLink.compose({
+  icon: 'arrow-left',
+  iconFirst: true,
+  className: 'view-link'
+});
