248e6c0f4291c5fd39f0ec06216c3a75e0e341ac
Riku Rouvila <riku.rouvila@gmail.com>
2015-07-05 16:39:24 +0300

    LIVIJUKU-253
    Poista angular-riippuvuudet requiren cachesta kun testit ajetaan uudelleen (--watch modessa)
    
    * Angular ja angular-mocks on molemmat singletoneja jotka initialisoidaan kerran sen hetkiseen window - objektiin
    * window-object luodaan kuitenkin joka kerta uudelleen kun testit ajetaan eikä mocha lataa uudelleen riippuvuuksia node_modules hakemistosta

--- tests/test-helpers.js
+++ tests/test-helpers.js
@@ -11,6 +11,16 @@ global.window.afterEach = afterEach;
 
 global.navigator = window.navigator = {};
 
+/*
+ * Since angular and angular-mocks are both singletons created once with one window-object
+ * and mocha doesn't reload modules from node_modules on watch mode we'll have to invalidate the cached
+ * singletons manually.
+ */
+
+delete require.cache[require.resolve('angular')];
+delete require.cache[require.resolve('angular/angular')];
+delete require.cache[require.resolve('angular-mocks')];
+
 require('angular/angular');
 require('angular-mocks');
 
