c204560b8288ee82a46946a5a5d766d2660c28ce
jegranbacka <jan-erik.granbacka@solita.fi>
2015-07-10 15:20:33 +0300

    LIVIJUKU-330 / Path ohjaamaan käyttäjä omalle aloitussivulleen * Hakija "omat hakemukset" sivulle ja käsittelijä "Hakemuskaudet" sivulle Route-määrityksiin tarkastus käyttäjän oikeuksista * Käyttäjä ohjautuu omalle aloitussivulleen mikäli oikeudet eivät riitä /h/hakemukset/:tyyppi route on nyt /y/hakemukset/:tyyppi (yhteiset)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,124 +18,124 @@ require('angular-ui-validate');
 require('angular-i18n/angular-locale_fi-fi');
 
 angular
-    .module('jukufrontApp', [
-        'services.auth',
-        'services.avustuskohde',
-        'services.hakemus',
-        'services.hakemuskausi',
-        'services.kayttaja',
-        'services.liite',
-        'services.organisaatio',
-        'services.suunnittelu',
-        'services.status',
-        'services.paatos',
-        'services.common',
-        'ngResource',
-        'angular-loading-bar',
-        'smart-table',
-        'ui.bootstrap',
-        'ui.bootstrap.tooltip',
-        'ui.bootstrap.dropdown',
-        'monospaced.elastic',
-        'ngFileUpload',
-        'toastr',
-        'ngAnimate',
-        'ngRoute',
-        'ng-currency',
-        'ui.bootstrap.showErrors',
-        'ui.validate'
-    ])
-    .config(['$routeProvider', function ($routeProvider) {
-      $routeProvider
-        .when('/', {
-          template: '<div></div>',
-          controller: ['$location', 'KayttajaService', function($location, KayttajaService) {
-            KayttajaService.hae().then(function(user) {
-              $location.path(defaultView(user));
-            });
-          }]
-        })
-        .when('/y/hakemukset/:tyyppi', {
-          template: require('views/kasittelija/hakemukset.html'),
-          controller: 'KasittelijaHakemuksetCtrl'
-        })
-        .when('/h/hakemus/:vuosi/:tyyppi/:id/:m1id/:m2id', restrictRoute(isHakija, {
-          template: require('views/hakija/hakemus.html'),
-          controller: 'HakemusCtrl'
-        }))
-        .when('/h/hakemukset', restrictRoute(isHakija, {
-          template: require('views/hakija/hakemukset.html'),
-          controller: 'HakijaHakemuksetCtrl'
-        }))
-        .when('/k/hakemus/:vuosi/:tyyppi/:id/:m1id/:m2id', restrictRoute(isKasittelija, {
-          template: require('views/kasittelija/hakemus.html'),
-          controller: 'HakemusCtrl'
-        }))
-        .when('/k/hakemuskaudenhallinta', restrictRoute(isKasittelija, {
-          template: require('views/kasittelija/hakemuskaudenHallinta.html'),
-          controller: 'KasittelijaHakemuskaudenHallintaCtrl'
-        }))
-        .when('/k/paatos/:vuosi/:tyyppi/:lajitunnus/:hakemusid/:haettuavustus/:avustus', restrictRoute(isKasittelija, {
-          template: require('views/kasittelija/paatos.html'),
-          controller: 'KasittelijaPaatosCtrl'
-        }))
-        .when('/k/suunnittelu/:vuosi/:tyyppi/:lajitunnus', restrictRoute(isKasittelija, {
-          template: require('views/kasittelija/suunnittelu.html'),
-          controller: 'KasittelijaSuunnitteluCtrl'
-        }))
-        .otherwise({
-          redirectTo: '/'
-        });
-    }])
-    .config(['$httpProvider', function ($httpProvider) {
-        //http://stackoverflow.com/questions/16098430/angular-ie-caching-issue-for-http
-        //initialize get if not there
-        if (!$httpProvider.defaults.headers.get) {
-            $httpProvider.defaults.headers.get = {};
-        }
+  .module('jukufrontApp', [
+    'services.auth',
+    'services.avustuskohde',
+    'services.hakemus',
+    'services.hakemuskausi',
+    'services.kayttaja',
+    'services.liite',
+    'services.organisaatio',
+    'services.suunnittelu',
+    'services.status',
+    'services.paatos',
+    'services.common',
+    'ngResource',
+    'angular-loading-bar',
+    'smart-table',
+    'ui.bootstrap',
+    'ui.bootstrap.tooltip',
+    'ui.bootstrap.dropdown',
+    'monospaced.elastic',
+    'ngFileUpload',
+    'toastr',
+    'ngAnimate',
+    'ngRoute',
+    'ng-currency',
+    'ui.bootstrap.showErrors',
+    'ui.validate'
+  ])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        template: '<div></div>',
+        controller: ['$location', 'KayttajaService', function ($location, KayttajaService) {
+          KayttajaService.hae().then(function (user) {
+            $location.path(defaultView(user));
+          });
+        }]
+      })
+      .when('/y/hakemukset/:tyyppi', {
+        template: require('views/kasittelija/hakemukset.html'),
+        controller: 'KasittelijaHakemuksetCtrl'
+      })
+      .when('/h/hakemus/:vuosi/:tyyppi/:id/:m1id/:m2id', restrictRoute(isHakija, {
+        template: require('views/hakija/hakemus.html'),
+        controller: 'HakemusCtrl'
+      }))
+      .when('/h/hakemukset', restrictRoute(isHakija, {
+        template: require('views/hakija/hakemukset.html'),
+        controller: 'HakijaHakemuksetCtrl'
+      }))
+      .when('/k/hakemus/:vuosi/:tyyppi/:id/:m1id/:m2id', {
+        template: require('views/kasittelija/hakemus.html'),
+        controller: 'HakemusCtrl'
+      })
+      .when('/k/hakemuskaudenhallinta', restrictRoute(isKasittelija, {
+        template: require('views/kasittelija/hakemuskaudenHallinta.html'),
+        controller: 'KasittelijaHakemuskaudenHallintaCtrl'
+      }))
+      .when('/k/paatos/:vuosi/:tyyppi/:lajitunnus/:hakemusid/:haettuavustus/:avustus', restrictRoute(isKasittelija, {
+        template: require('views/kasittelija/paatos.html'),
+        controller: 'KasittelijaPaatosCtrl'
+      }))
+      .when('/k/suunnittelu/:vuosi/:tyyppi/:lajitunnus', restrictRoute(isKasittelija, {
+        template: require('views/kasittelija/suunnittelu.html'),
+        controller: 'KasittelijaSuunnitteluCtrl'
+      }))
+      .otherwise({
+        redirectTo: '/'
+      });
+  }])
+  .config(['$httpProvider', function ($httpProvider) {
+    //http://stackoverflow.com/questions/16098430/angular-ie-caching-issue-for-http
+    //initialize get if not there
+    if (!$httpProvider.defaults.headers.get) {
+      $httpProvider.defaults.headers.get = {};
+    }
 
-        // Answer edited to include suggestions from comments
-        // because previous version of code introduced browser-related errors
+    // Answer edited to include suggestions from comments
+    // because previous version of code introduced browser-related errors
 
-        //disable IE ajax request caching
-        $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
-        // extra
-        $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
-        $httpProvider.defaults.headers.Pragma = 'no-cache';
-    }])
-    .run(['$rootScope', '$location', function($rootScope, $location) {
-      $rootScope.$on('$routeChangeError', () => $location.path('/'));
-    }])
-    .directive('jukuHeader', require('components/header'))
-    .directive('jukuSidebar', require('components/sidebar'))
-    .directive('jukuNavigation', require('components/navigation'))
-    .directive('jukuFooter', require('components/footer'))
-    .directive('jukuTabs', require('components/tabs'))
-    .directive('jukuTab', require('components/tab'))
-    .directive('jukuBadge', require('components/badge'))
-    .directive('jukuPoistumisvaroitus', require('components/poistumisvaroitus'))
-    .directive('jukuVarmistusdialogi', require('components/varmistusdialogi'))
-    .directive('jukuTaydennysdialogi', require('components/taydennysdialogi'))
-    .directive('hakemuksenTila', require('components/hakemuksenTila'))
-    .directive('hakemusSummary', require('components/hakemusSummary'))
-    .directive('hakemusPanel', require('components/hakemusPanel'))
-    .directive('hakemuskausiPanel', require('components/hakemuskausiPanel'))
-    .directive('hakemusLaatikko', require('components/hakemusLaatikko'))
-    .directive('jukuOhje', require('components/ohje'))
-    .directive('jukuLiiteLataus', require('components/liiteLataus'))
-    .directive('jukuLiiteTarkistaminen', require('components/liiteTarkistaminen'))
-    .directive('jukuUserDropdown', require('components/userDropdown'))
-    .directive('jukuLinkNext', require('components/navigationLink').next)
-    .directive('jukuLinkPrev', require('components/navigationLink').prev)
-    .directive('jukuFormSection', require('components/formSection'))
-    .directive('jukuFormRow', require('components/formRow'))
-    .directive('jukuForm', require('components/form'))
-    .directive('jukuIconLink', require('components/iconLink'))
-    .directive('jukuFileDetails', require('components/fileDetails'))
-    .directive('jukuFileActions', require('components/fileActions'))
-    .directive('jukuCheckbox', require('components/checkbox'))
-    .directive('jukuAvustuskohde', require('components/avustuskohde'))
-    .directive('hakemusLabel', require('components/hakemusLabel'));
+    //disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    // extra
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.Pragma = 'no-cache';
+  }])
+  .run(['$rootScope', '$location', function ($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', () => $location.path('/'));
+  }])
+  .directive('jukuHeader', require('components/header'))
+  .directive('jukuSidebar', require('components/sidebar'))
+  .directive('jukuNavigation', require('components/navigation'))
+  .directive('jukuFooter', require('components/footer'))
+  .directive('jukuTabs', require('components/tabs'))
+  .directive('jukuTab', require('components/tab'))
+  .directive('jukuBadge', require('components/badge'))
+  .directive('jukuPoistumisvaroitus', require('components/poistumisvaroitus'))
+  .directive('jukuVarmistusdialogi', require('components/varmistusdialogi'))
+  .directive('jukuTaydennysdialogi', require('components/taydennysdialogi'))
+  .directive('hakemuksenTila', require('components/hakemuksenTila'))
+  .directive('hakemusSummary', require('components/hakemusSummary'))
+  .directive('hakemusPanel', require('components/hakemusPanel'))
+  .directive('hakemuskausiPanel', require('components/hakemuskausiPanel'))
+  .directive('hakemusLaatikko', require('components/hakemusLaatikko'))
+  .directive('jukuOhje', require('components/ohje'))
+  .directive('jukuLiiteLataus', require('components/liiteLataus'))
+  .directive('jukuLiiteTarkistaminen', require('components/liiteTarkistaminen'))
+  .directive('jukuUserDropdown', require('components/userDropdown'))
+  .directive('jukuLinkNext', require('components/navigationLink').next)
+  .directive('jukuLinkPrev', require('components/navigationLink').prev)
+  .directive('jukuFormSection', require('components/formSection'))
+  .directive('jukuFormRow', require('components/formRow'))
+  .directive('jukuForm', require('components/form'))
+  .directive('jukuIconLink', require('components/iconLink'))
+  .directive('jukuFileDetails', require('components/fileDetails'))
+  .directive('jukuFileActions', require('components/fileActions'))
+  .directive('jukuCheckbox', require('components/checkbox'))
+  .directive('jukuAvustuskohde', require('components/avustuskohde'))
+  .directive('hakemusLabel', require('components/hakemusLabel'));
 
 require('./controllers/hakija/hakemukset');
 require('./controllers/kasittelija/hakemukset');
