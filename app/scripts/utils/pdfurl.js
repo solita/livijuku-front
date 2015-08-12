86a4af95817edb646a2a58fa25c1500cb36b72b6
jegranbacka <jan-erik.granbacka@solita.fi>
2015-08-12 09:52:44 +0300

    LIVIJUKU-394 PDF-vieweriä ei löydy esikatselupainiketta painaessa.

--- app/scripts/utils/pdfurl.js
+++ app/scripts/utils/pdfurl.js
@@ -2,15 +2,15 @@
 
 module.exports = {
   getHakemusPdfUrl: function (id) {
-    return '/pdf/web/viewer.html?file=/api/hakemus/' + id + '/pdf';
+    return 'pdf/web/viewer.html?file=../../api/hakemus/' + id + '/pdf';
   },
   getPaatosPdfUrl: function (id) {
-    return '/pdf/web/viewer.html?file=/api/hakemus/' + id + '/paatos/pdf';
+    return 'pdf/web/viewer.html?file=../../api/hakemus/' + id + '/paatos/pdf';
   },
   getHakuohjePdfUrl: function (vuosi) {
-    return '/pdf/web/viewer.html?file=/api/hakemuskausi/' + vuosi + '/hakuohje';
+    return 'pdf/web/viewer.html?file=../../api/hakemuskausi/' + vuosi + '/hakuohje';
   },
   getLiitePdfUrl: function (hakemusid, liitenumero) {
-    return '/pdf/web/viewer.html?file=/api/hakemus/' + hakemusid + '/liite/' + liitenumero;
+    return 'pdf/web/viewer.html?file=../../api/hakemus/' + hakemusid + '/liite/' + liitenumero;
   }
 };
