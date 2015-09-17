62ce9747286740013a35ee146f75044fbbe050a8
Petri Sirkkala <sirpete@iki.fi>
2015-09-17 10:23:08 +0300

    LIVIJUKU-431:TLP TESTI: Laajennetaan "hakijaa ihformoidaan hakemuksen vaiheista" kattamaan myös maksatushakemukset. - Muutettu firstFileInput -> uniqueFileInput.   -- Tarkoitus oli aina etsiä sivulta ainoa <input type=file>, joten nyt varmistetaan, että löytyy tasan yksi elementti.

--- livijuku-front-e2e-tests/src/test/java/yhteinen/Hakemus.java
+++ livijuku-front-e2e-tests/src/test/java/yhteinen/Hakemus.java
@@ -1,6 +1,10 @@
 package yhteinen;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
+
+import com.thoughtworks.selenium.SeleneseTestNgHelper;
 
 import juku.e2e.TestBase;
 import util.ViewBase;
@@ -9,8 +13,15 @@ import util.ViewBase;
  * Created by petrisi on 16.9.15.
  */
 public class Hakemus extends ViewBase {
-    public static WebElement firstFileInput() {
-        return TestBase.driver.findElementByXPath("//input[@type='file']");
+    public static WebElement uniqueFileInput() {
+        List<WebElement> fileInputs = TestBase.driver.findElementsByXPath("//input[@type='file']");
+        if (fileInputs.size()>1) {
+            SeleneseTestNgHelper.fail("Sivulla on useita //input[@type='file'] elementtejä. Odotettiin täsmälleen yhtä.");
+        }
+        else if (fileInputs.size()==0) {
+            SeleneseTestNgHelper.fail("Sivulla ei ole yhtään //input[@type='file'] elementtiä. Odotettiin täsmälleen yhtä.");
+        }
+        return fileInputs.get(0);
     }
 
     public static WebElement tallennaHakemus() {
