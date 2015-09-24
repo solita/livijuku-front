8d85f71c9bbebf61ca4d9443ce89e6069344f720
jegranbacka <jan-erik.granbacka@solita.fi>
2015-09-24 14:37:23 +0300

    LIVIJUKU-415 Mobilinäkymän (kapean näkymän) Tunnusluku-linkit ei toimi.

--- app/scripts/components/sidebar/index.js
+++ app/scripts/components/sidebar/index.js
@@ -1,12 +1,16 @@
 'use strict';
 
-function sidebarController($scope) {
+function sidebarController($scope, ConfigService) {
   $scope.sallittu = require('utils/hasPermission');
+
+  ConfigService.hae().then(function (response) {
+    $scope.logoutUrl = response.logoutUrl;
+  }, $scope.logoutUrl = '#/');
 }
 
-sidebarController.$inject = ['$scope'];
+sidebarController.$inject = ['$scope', 'ConfigService'];
 
-module.exports = function() {
+module.exports = function () {
   return {
     restrict: 'E',
     template: require('./index.html'),
