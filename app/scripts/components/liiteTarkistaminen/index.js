2c603c0e0150650981bfe8ac9fffa1ced98ea16f
jegranbacka <jan-erik.granbacka@solita.fi>
2015-10-07 10:21:49 +0300

    LIVIJUKU-475 Hakemuksen liitteiden kirjoitusasun muutos (tiloissa, joissa hakemus ei ole editoitavissa)

--- app/scripts/components/liiteTarkistaminen/index.js
+++ app/scripts/components/liiteTarkistaminen/index.js
@@ -1,11 +1,24 @@
 'use strict';
 var _ = require('lodash');
+var pdf = require('utils/pdfurl');
 
 function liitetarkistaminenController(LiiteService, $scope, StatusService) {
 
   $scope.liitteetOlemassa = function () {
     if (typeof $scope.liitteet === 'undefined') return false;
     return $scope.liitteet.length > 0;
+  };
+
+  $scope.formatFileSize = function (size) {
+    var sizes = [' B', ' KB', ' MB', ' GB', ' TB', ' PB', ' EB', ' ZB', ' YB'];
+    for (var i = 1; i < sizes.length; i++) {
+      if (size < Math.pow(1024, i)) return (Math.round((size / Math.pow(1024, i - 1)) * 100) / 100) + sizes[i - 1];
+    }
+    return size;
+  };
+
+  $scope.getLiitePdf = function (hakemusid, liitenumero) {
+    return pdf.getLiitePdfUrl(hakemusid, liitenumero);
   };
 
   LiiteService.haeKaikki($scope.hakemusid).then(function (response) {
