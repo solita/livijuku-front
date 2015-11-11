81de2880215ea11962cbb8fb2f5cb13b91abb74f
jegranbacka <jan-erik.granbacka@solita.fi>
2015-11-11 12:19:58 +0200

    LIVIJUKU-541: ELY-hakemukset: kaikki hakemukset -näkymään ELY-keskus-välilehti ja sisältö

--- app/scripts/components/header/index.js
+++ app/scripts/components/header/index.js
@@ -1,4 +1,5 @@
 'use strict';
+var _ = require('lodash');
 
 function headerController($scope, $rootScope, $stateParams, $filter, KayttajaService, HakemusService) {
   $scope.sallittu = require('utils/hasPermission');
@@ -11,27 +12,35 @@ function headerController($scope, $rootScope, $stateParams, $filter, KayttajaSer
   });
 
   $rootScope.$on('$stateChangeSuccess', (event, newState) => {
-    if(newState.name === 'app.hakemus') {
+    if (newState.name === 'app.hakemus') {
       HakemusService.hae($stateParams.id)
-      .then((hakemus) => {
-        $scope.hakemus = hakemus;
-      });
+        .then((hakemus) => {
+          $scope.hakemus = hakemus;
+        });
     }
     $scope.hakemus = null;
   });
 
-  $scope.toggleCollapse = function() {
+  $scope.toggleCollapse = function () {
     $rootScope.isCollapsed = !$rootScope.isCollapsed;
   };
 
-  $scope.isOwnApplication = function() {
-    if(!($scope.user && $scope.hakemus)) {
+  $scope.isOwnApplication = function () {
+    if (!($scope.user && $scope.hakemus)) {
       return false;
     }
     return $scope.hakemus.organisaatioid === $scope.user.organisaatioid;
   };
 
-  $scope.ownApplicationTabActive = function() {
+  $scope.getType = function () {
+    if ((typeof $rootScope.organisaatiot) === 'undefined') return;
+    if (_.find($rootScope.organisaatiot, {'id': $scope.user.organisaatioid}).lajitunnus === 'ELY') {
+      return 'ELY';
+    } else
+      return 'AH0';
+  };
+
+  $scope.ownApplicationTabActive = function () {
     return $filter('isState')('app.hakija.hakemukset.omat') ||
       ($filter('isState')('app.hakemus') && $scope.isOwnApplication());
   };
@@ -39,7 +48,7 @@ function headerController($scope, $rootScope, $stateParams, $filter, KayttajaSer
 
 headerController.$inject = ['$scope', '$rootScope', '$stateParams', '$filter', 'KayttajaService', 'HakemusService'];
 
-module.exports = function() {
+module.exports = function () {
   return {
     restrict: 'E',
     template: require('./index.html'),
