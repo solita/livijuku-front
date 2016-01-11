ccbe471abf71cd87f30242e5ab96649e15236c32
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2016-01-11 16:27:40 +0200

    LIVIJUKU-679 - tunnusluvut - kysyntä- ja tarjontatietojen palvelut.

---
+++ app/scripts/services/tunnusluku.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var angular = require('angular');
+var _ = require('lodash');
+
+function liikennetilastotURL(vuosi, organisaatioid, sopimustyyppitunnus) {
+  return 'api/liikennetilastot/' + vuosi + '/' + organisaatioid  '/' + sopimustyyppitunnus;
+}
+
+angular.module('services.tunnusluku', [])
+
+  .factory('TunnuslukuEditService', ['$http', function ($http) {
+    return {
+      haeKysyntaTarjonta: function (vuosi, organisaatioid, sopimustyyppitunnus) {
+        return $http.get(liikennetilastotURL(vuosi, organisaatioid, sopimustyyppitunnus)).then(res => res.data);
+      },
+      tallennaKysyntaTarjonta: function (vuosi, organisaatioid, sopimustyyppitunnus, kuukaudet) {
+        return $http.put(liikennetilastotURL(vuosi, organisaatioid, sopimustyyppitunnus), kuukaudet);
+      }
+    };
+  }]);
