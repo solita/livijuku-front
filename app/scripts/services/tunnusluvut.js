e5a375ab0ca05d11b7109e85c3681756fff5a54a
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2016-01-19 12:57:29 +0200

    LIVIJUKU-679 - helpompi tapa luoda tunnuslukuihin liittyvät saman mallin mukaiset palvelut

--- app/scripts/services/tunnusluvut.js
+++ app/scripts/services/tunnusluvut.js
@@ -4,65 +4,56 @@ var angular = require('angular');
 var _ = require('lodash');
 var t = require('utils/tunnusluvut');
 
-function liikennetilastotURL(vuosi, organisaatioid, sopimustyyppitunnus) {
-  return 'api/liikennetilastot/' + vuosi + '/' + organisaatioid + '/' + sopimustyyppitunnus;
-}
-
-function liikenneviikkoURL(vuosi, organisaatioid, sopimustyyppitunnus) {
-  return 'api/liikenneviikko/' + vuosi + '/' + organisaatioid + '/' + sopimustyyppitunnus;
-}
-
-function kalustoURL(vuosi, organisaatioid, sopimustyyppitunnus) {
-  return 'api/kalusto/' + vuosi + '/' + organisaatioid + '/' + sopimustyyppitunnus;
+function tunnuslukuURL(tunnusluku, vuosi, organisaatioid, sopimustyyppitunnus) {
+  return 'api/' + tunnusluku + '/' + vuosi + '/' + organisaatioid  + '/' + sopimustyyppitunnus;
 }
 
 angular.module('services.tunnusluvut', [])
 
   .factory('TunnuslukuEditService', ['$http', function ($http) {
-    return {
-      haeKysyntaTarjonta: function (vuosi, organisaatioid, sopimustyyppitunnus) {
-        return $http.get(liikennetilastotURL(vuosi, organisaatioid, sopimustyyppitunnus)).then(res =>
-          res.data.length === 0 ?
-            _.map(_.range(1, 13), kuukausi =>
-              ({
-                kuukausi: kuukausi,
-                nousut: null,
-                lahdot: null,
-                linjakilometrit: null
-              })) :
-            res.data);
-      },
-      tallennaKysyntaTarjonta: function (vuosi, organisaatioid, sopimustyyppitunnus, kuukaudet) {
-        return $http.put(liikennetilastotURL(vuosi, organisaatioid, sopimustyyppitunnus), kuukaudet);
-      },
 
-      haeKysyntaTarjontaViikko: function (vuosi, organisaatioid, sopimustyyppitunnus) {
-        return $http.get(liikenneviikkoURL(vuosi, organisaatioid, sopimustyyppitunnus)).then(res =>
-          res.data.length === 0 ?
-            _.map(t.viikonpaivaluokat, tunnus =>
-              ({
-                viikonpaivaluokkatunnus: tunnus,
-                nousut: null,
-                lahdot: null,
-                linjakilometrit: null
-              })) :
-            res.data);
-      },
-      tallennaKysyntaTarjontaViikko: function (vuosi, organisaatioid, sopimustyyppitunnus, viikko) {
-        return $http.put(liikenneviikkoURL(vuosi, organisaatioid, sopimustyyppitunnus), viikko);
-      },
-      haeKalusto: function (vuosi, organisaatioid, sopimustyyppitunnus) {
-        return $http.get(kalustoURL(vuosi, organisaatioid, sopimustyyppitunnus)).then(res =>
-          res.data.length === 0 ?
-            _.map(t.paastoluokat, tunnus =>
-              ({
-                paastoluokkatunnus: tunnus,
-                lukumaara: null
-              })) :
-            res.data);
-      },
-      tallennaKalusto: function (vuosi, organisaatioid, sopimustyyppitunnus, kalusto) {
-        return $http.put(kalustoURL(vuosi, organisaatioid, sopimustyyppitunnus), kalusto);
-      }
+    function createHakuOperation(tunnusluku, defaultdata) {
+      return function (vuosi, organisaatioid, sopimustyyppitunnus) {
+          return $http.get(tunnuslukuURL(tunnusluku, vuosi, organisaatioid, sopimustyyppitunnus))
+            .then(res =>
+              res.data.length === 0 ?
+                defaultdata(vuosi, organisaatioid, sopimustyyppitunnus) :
+                res.data);
+        };
+    }
+
+    function createTallennaOperation(tunnusluku) {
+      return function (vuosi, organisaatioid, sopimustyyppitunnus, kalusto) {
+        return $http.put(tunnuslukuURL(tunnusluku, vuosi, organisaatioid, sopimustyyppitunnus), kalusto);
+      };
+    }
+
+    return {
+      // liikennevuosi
+      haeKysyntaTarjonta: createHakuOperation('liikennetilastot',
+          () => _.map(_.range(1, 13), kuukausi => ({
+                        kuukausi: kuukausi,
+                        nousut: null,
+                        lahdot: null,
+                        linjakilometrit: null}))),
+      tallennaKysyntaTarjonta: createTallennaOperation('liikennetilastot'),
+
+      // liikenneviikko
+      haeKysyntaTarjontaViikko: createHakuOperation('liikenneviikko',
+          () => _.map(t.viikonpaivaluokat, tunnus => ({
+                        viikonpaivaluokkatunnus: tunnus,
+                        nousut: null,
+                        lahdot: null,
+                        linjakilometrit: null
+                      }))),
+      tallennaKysyntaTarjontaViikko: createTallennaOperation('liikenneviikko'),
+
+      // kalusto
+      haeKalusto: createHakuOperation('kalusto',
+          () => _.map(t.paastoluokat, tunnus => ({
+                        paastoluokkatunnus: tunnus,
+                        lukumaara: null
+                      }))),
+      tallennaKalusto: createTallennaOperation('kalusto'),
     };
   }]);
