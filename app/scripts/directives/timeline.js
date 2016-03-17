8e226ae11a58890fe452164613e0be10307e56fe
rapitkan <rapitkan@gmail.com>
2016-03-17 17:01:21 +0200

    LIVIJUKU-789 Kilpailutuksen/kohteen nimi näkyy tämän muutoksen myötä ruudulla silloin, kun edes osa kohteesta on aikajanalla näkyvissä.

--- app/scripts/directives/timeline.js
+++ app/scripts/directives/timeline.js
@@ -17,23 +17,23 @@ angular.module('jukufrontApp')
           colors = [
             { 0: '#0085c9', 1: '#ffffff' },
             { 0: 'black', 1: '#ffffff' },
-            { 0: '#f2f2f2', 1: 'black' },
+            { 0: 'brown', 1: '#ffffff' },
             { 0: 'grey', 1: '#ffffff' },
             { 0: 'red', 1: '#ffffff' },
             { 0: 'orange', 1: '#ffffff' }
-          ];
+          ], i, ii;
 
         scope.$watch('organisaatiot', function(value) {
           if (scope.organisaatiot && Object.keys(scope.organisaatiot).length) {
-            for (var i = 0; i < scope.organisaatiot.length; i += 1) {
+            for (i = 0; i < scope.organisaatiot.length; i += 1) {
               groups.push({
                 id: scope.organisaatiot[i].id,
                 content: scope.organisaatiot[i].nimi
               });
             }
 
-            for (var i = 0; i < scope.kilpailutukset.length; i += 1) {
-              for (var ii = 0; ii < scope.kilpailutukset[i].dates.length - 1; ii += 1) {
+            for (i = 0; i < scope.kilpailutukset.length; i += 1) {
+              for (ii = 0; ii < scope.kilpailutukset[i].dates.length - 1; ii += 1) {
                 items.push({
                   id: scope.kilpailutukset[i].organisaatioId + '-' + scope.kilpailutukset[i].id + '-' + ii,
                   content: ii === 0 ? scope.kilpailutukset[i].name : '&nbsp;',
@@ -42,9 +42,20 @@ angular.module('jukufrontApp')
                   group: scope.kilpailutukset[i].organisaatioId,
                   subgroup: scope.kilpailutukset[i].id,
                   title: scope.kilpailutukset[i].name,
-                  style: 'background-color: ' + (colors[ii][0] ? colors[ii][0] : 'brown') + '; color: ' + (colors[ii][1] ? colors[ii][1] : '#ffffff') + '; border: none;' +  (ii === 0 ? ' z-index: 2;' : '')
+                  style: 'background-color: ' + (colors[ii][0] ? colors[ii][0] : 'brown') + '; color: ' + (colors[ii][1] ? colors[ii][1] : '#ffffff') + '; border: none;'
                 });
               }
+
+              items.push({
+                id: scope.kilpailutukset[i].organisaatioId + '-' + scope.kilpailutukset[i].id + '-' + ii,
+                content: scope.kilpailutukset[i].name,
+                start: scope.kilpailutukset[i].dates[0],
+                end: scope.kilpailutukset[i].dates[ii],
+                group: scope.kilpailutukset[i].organisaatioId,
+                subgroup: scope.kilpailutukset[i].id,
+                title: scope.kilpailutukset[i].name,
+                style: 'background-color: transparent; color: white; border: none; z-index: 2;'
+              });
             }
 
             timeline.setOptions(scope.options);
