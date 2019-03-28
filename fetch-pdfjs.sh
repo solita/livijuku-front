0f35a190a538a96017e24f0ae4582d954cde6b7f
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2019-03-28 09:27:24 +0200

    LIVIJUKU-1060 - system version increment.

--- fetch-pdfjs.sh
+++ fetch-pdfjs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 (
   set +o noclobber
-  curl -sSL -o- http://jenkins.livijuku.solita.fi/job/pdfjs/lastSuccessfulBuild/artifact/build/generic/*zip*/generic.zip >generic.zip
+  #curl -sSL -o- http://jenkins.livijuku.solita.fi/job/pdfjs/lastSuccessfulBuild/artifact/build/generic/*zip*/generic.zip >generic.zip
   rm -rf dist/pdf dist/generic
   cd dist
   unzip -qq ../generic.zip
