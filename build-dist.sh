f7a10a37765ff2fa87884290a6fc69885bad019b
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2019-06-10 16:58:20 +0300

    nvm has to be used

--- build-dist.sh
+++ build-dist.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
-# Lisatty Windows/Cygwin:a varten sulut ja export
-(
-  export NODE_PATH=app/scripts
-  npm install
-  npm prune
-  export NODE_ENV=production
-  rm -rf dist
-  npm run build
-  ./unzip-pdfjs.sh
-)
+#!/usr/bin/env bash
+set -e
+
+# if nvm does not exist try to load it from default location
+which nvm || source ~/.nvm/nvm.sh
+
+nvm use
+export NODE_PATH=app/scripts
+npm install
+npm prune
+export NODE_ENV=production
+rm -rf dist
+npm run build
+./unzip-pdfjs.sh
+
