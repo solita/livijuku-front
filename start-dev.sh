2195497d16b3e42b55181bbf5adffb6d726372cb
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2019-06-12 16:12:18 +0300

    use nvm

--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# if nvm does not exist try to load it from default location
+which nvm || source ~/.nvm/nvm.sh
+
+nvm use
 rm -rf dist
 mkdir dist
 ./unzip-pdfjs.sh
