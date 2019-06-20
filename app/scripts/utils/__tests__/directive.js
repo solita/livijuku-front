8e2445a81e965b08e068ad2b7ec04d94c9f80100
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2019-06-21 01:04:45 +0300

    LIVIJUKU-1054 - remove jasmine (not used) + fix unit tests

--- app/scripts/utils/__tests__/directive.js
+++ app/scripts/utils/__tests__/directive.js
@@ -17,16 +17,16 @@ describe('Directive utility functions', function() {
 
     assert.equal(t.formatFloat(1), '1');
     assert.equal(t.formatFloat(1.1), '1,1');
-    assert.equal(t.formatFloat(1111), '1 111');
-    assert.equal(t.formatFloat(11111), '11 111');
-    assert.equal(t.formatFloat(111111), '111 111');
-    assert.equal(t.formatFloat(1111111), '1 111 111');
+    assert.equal(t.formatFloat(1111), '1\xa0111');
+    assert.equal(t.formatFloat(11111), '11\xa0111');
+    assert.equal(t.formatFloat(111111), '111\xa0111');
+    assert.equal(t.formatFloat(1111111), '1\xa0111\xa0111');
 
-    assert.equal(t.formatFloat(1111.1), '1 111,1');
-    assert.equal(t.formatFloat(1111.11), '1 111,11');
-    assert.equal(t.formatFloat(1111.111), '1 111,111');
-    assert.equal(t.formatFloat(1111.1111), '1 111,1111');
-    assert.equal(t.formatFloat(1111.11111), '1 111,11111');
+    assert.equal(t.formatFloat(1111.1), '1\xa0111,1');
+    assert.equal(t.formatFloat(1111.11), '1\xa0111,11');
+    assert.equal(t.formatFloat(1111.111), '1\xa0111,111');
+    assert.equal(t.formatFloat(1111.1111), '1\xa0111,1111');
+    assert.equal(t.formatFloat(1111.11111), '1\xa0111,11111');
 
   });
 
@@ -36,16 +36,16 @@ describe('Directive utility functions', function() {
     assert.equal(t.formatFloatToFixed(3, 1), '1,000');
 
     assert.equal(t.formatFloatToFixed(2, 1.1), '1,10');
-    assert.equal(t.formatFloatToFixed(2, 1111), '1 111,00');
-    assert.equal(t.formatFloatToFixed(2, 11111), '11 111,00');
-    assert.equal(t.formatFloatToFixed(2, 111111), '111 111,00');
-    assert.equal(t.formatFloatToFixed(2, 1111111), '1 111 111,00');
+    assert.equal(t.formatFloatToFixed(2, 1111), '1\xa0111,00');
+    assert.equal(t.formatFloatToFixed(2, 11111), '11\xa0111,00');
+    assert.equal(t.formatFloatToFixed(2, 111111), '111\xa0111,00');
+    assert.equal(t.formatFloatToFixed(2, 1111111), '1\xa0111\xa0111,00');
 
-    assert.equal(t.formatFloatToFixed(2, 1111.1), '1 111,10');
-    assert.equal(t.formatFloatToFixed(2, 1111.11), '1 111,11');
-    assert.equal(t.formatFloatToFixed(2, 1111.111), '1 111,11');
-    assert.equal(t.formatFloatToFixed(2, 1111.1111), '1 111,11');
-    assert.equal(t.formatFloatToFixed(2, 1111.11111), '1 111,11');
+    assert.equal(t.formatFloatToFixed(2, 1111.1), '1\xa0111,10');
+    assert.equal(t.formatFloatToFixed(2, 1111.11), '1\xa0111,11');
+    assert.equal(t.formatFloatToFixed(2, 1111.111), '1\xa0111,11');
+    assert.equal(t.formatFloatToFixed(2, 1111.1111), '1\xa0111,11');
+    assert.equal(t.formatFloatToFixed(2, 1111.11111), '1\xa0111,11');
 
 
     assert.equal(t.formatFloatToFixed(2, 100.005), '100,01');
